19b6be1f84d591b3f4e59196158a3378031b590e
Silvestre Fernandes Fernandes da Silva Neto <silvaneto.cc@gmail.com>
2025-05-30 08:46:58 -0300

    Atualizado

--- cliente-hm-back-main/src/main/java/com/clientehm/repository/AdministradorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.clientehm.repository;
-
-import com.clientehm.entity.AdministradorEntity; // Import da entidade no pacote correto
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface AdministradorRepository extends JpaRepository<AdministradorEntity, Long> {
-    Optional<AdministradorEntity> findByEmail(String email);
-}
