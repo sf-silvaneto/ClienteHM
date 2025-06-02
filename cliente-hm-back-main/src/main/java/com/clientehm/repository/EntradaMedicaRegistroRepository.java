680cb84907b1f2f3e93397239437b8495f0da11f
Silvestre Fernandes Fernandes da Silva Neto <silvaneto.cc@gmail.com>
2025-06-01 21:45:12 -0300

    Atualizado

--- cliente-hm-back-main/src/main/java/com/clientehm/repository/EntradaMedicaRegistroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.clientehm.repository;
-
-import com.clientehm.entity.EntradaMedicaRegistroEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EntradaMedicaRegistroRepository extends JpaRepository<EntradaMedicaRegistroEntity, Long> {
-}
